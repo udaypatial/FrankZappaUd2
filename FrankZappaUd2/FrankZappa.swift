94aa7962d18b1df5380db876ca0334c9bbbbf733
Uday P <uday.patial@thescore.com>
2022-03-23 15:59:18 -0400

    Add podspec

---
+++ FrankZappaUd2/FrankZappa.swift
@@ -0,0 +1,14 @@
+//
+//  FrankZappa.swift
+//  FrankZappaUd2
+//
+//  Created by Uday Patial on 2022-03-23.
+//
+
+public final class FrankZappa {
+	public init() {}
+	public func combineArrays(_ a: [Int], b: [Int]) -> [Int] {
+		return a + b
+	}
+}
+
--- FrankZappaUd2Tests/FrankZappaUd2Tests.swift
+++ FrankZappaUd2Tests/FrankZappaUd2Tests.swift
@@ -9,25 +9,17 @@ import XCTest
 @testable import FrankZappaUd2
 
 class FrankZappaUd2Tests: XCTestCase {
+	var subject: FrankZappa!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+	override func setUp() {
+		subject = FrankZappa()
+	}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+	func testAdd() {
+		let arr1 = [1, 2, 3]
+		let arr2 = [4, 5, 6]
+		let expected = [1, 2, 3, 4, 5, 6]
+		let actual = subject.combineArrays(arr1, b: arr2)
+		XCTAssertEqual(expected, actual)
+	}
 }
